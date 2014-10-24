bbf36f97b313d982dfbe5d29ec59e8202b6a81e0
perillaroc <perillaroc@gmail.com>
2014-10-24 11:16:48 +0800

    ADD: message class.

---
+++ sms_log_collector/message.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+
+
+class Message(object):
+    def __init__(self):
+        self.message_id = None
+        self.message_type = None
+        self.message_time = None
+        self.message_command = None
+        self.message_fullname = None
+        self.message_additional_information = None
+        self.message_string = None
+
+    def parse(self, line):
+        self.message_string = line
+        start_pos = 2
+        end_pos = line.find(':')
+        self.message_type = line[start_pos:end_pos]
+
+        start_pos = end_pos + 2
+        end_pos = line.find(']',start_pos)
+        message_time_string = line[start_pos:end_pos]
+        self.message_time = datetime.strptime(message_time_string, '%H:%M:%S %d.%m.%Y')
+
+        start_pos = end_pos + 2
+        end_pos = line.find(":", start_pos)
+        self.message_command = line[start_pos:end_pos]
+
+        if self.message_command in ('submitted', 'active', 'queued', 'complete', 'aborted'):
+            self.message_fullname = line[end_pos+1:].strip()
