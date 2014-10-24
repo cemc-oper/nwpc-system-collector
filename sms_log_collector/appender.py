9ec61a4bb7b234c03241e55542d5ccfa09af6707
perillaroc <perillaroc@gmail.com>
2014-10-24 15:25:07 +0800

    ADD: database engine to move sql from other files.

--- sms_log_collector/appender.py
+++ sms_log_collector/appender.py
@@ -4,36 +4,45 @@ from datetime import date, datetime, timedelta
 import mysql.connector
 
 from sms_log_collector.message import Message
+from sms_log_collector.database_engine import DatabaseEngine
 
 
 def main():
     print datetime.now()
 
-    user_name = 'nwp_qu'
+    user_name = 'nwp'
     host_name = 'cma20n03'
-    database_name = 'message_{user_name}_{host_name}'.format(user_name=user_name, host_name=host_name)
-    log_file_path = "/cma/u/{user_name}/smsworks/sms/{host_name}.sms.log".format(user_name=user_name, host_name=host_name)
+    table_name = 'message_{user_name}_{host_name}'.format(user_name=user_name, host_name=host_name)
+    log_file_path = "/cma/u/{user_name}/smsworks/sms/{host_name}.sms.log".format(user_name=user_name,
+                                                                                 host_name=host_name)
+    engine_config = {
+        'user': 'wangdp',
+        'password': 'shenyang',
+        'host': 'localhost',
+        'database': 'smslog',
+        'table_name': table_name
+    }
+
+    # check file
     if not os.path.isfile(log_file_path):
         print "log file doesn't exist: {log_file_path}".format(log_file_path=log_file_path)
         sys.exit(2)
 
     with open(log_file_path, 'r') as log_file:
-        connect = mysql.connector.connect(
-            user='wangdp',
-            password='shenyang',
-            host='localhost',
-            database='smslog'
-        )
-        cursor = connect.cursor()
-        print "Fetching the first message in database...",
-        query = ("SELECT message_id, message_string FROM {database_name} ORDER BY message_id LIMIT 1".format(
-            database_name=database_name
-        ))
-        cursor.execute(query)
         message_id = None
         message_string = None
-        for (message_id, message_string) in cursor:
+
+        engine = DatabaseEngine()
+        engine.create_connect(engine_config)
+        cursor = engine.create_cursor()
+
+        print "Fetching the first message in database...",
+        message_list = engine.get_message(order_by_string="ORDER BY message_id LIMIT 1")
+        if len(message_list) == 1:
+            message_id = message_list[0].message_id
+            message_string = message_list[0].message_string
             print "Found"
+
         pos = 0
         if message_id is None:
             print "There is no message in database"
@@ -48,27 +57,26 @@ def main():
                 print message_string,
                 sys.exit()
             print "Matched."
+
             print "Fetching the count of messages in database...",
-            query = ("SELECT COUNT(message_id) FROM {database_name}".format(database_name=database_name))
-            cursor.execute(query)
-            for (count,) in cursor:
-                print count
+            count = engine.get_message_count()
+            print count
+
             print "Fetching the last message in database...",
-            query = ("SELECT message_id, message_string FROM {database_name} ORDER BY message_id DESC LIMIT 1".format(
-                database_name=database_name
-            ))
-            cursor.execute(query)
-            for (message_id, message_string) in cursor:
+            message_list = engine.get_message(order_by_string="ORDER BY message_id DESC LIMIT 1")
+            if len(message_list) == 1:
+                message_id = message_list[0].message_id
+                message_string = message_list[0].message_string
                 print "Done"
-            print "Searching for line No. {count}...".format(count=count),
-            for i in range(1, int(count)-1):
+
+            print "Searching the log file for line No. {count}...".format(count=count),
+            for i in range(pos, int(count)-1):
                 log_file.readline()
             line = log_file.readline()
             print "Found"
             print "line          :", line,
             print "message_string:", message_string,
 
-        append_line_count = 0
         print 'read all lines that are not in database...',
         lines = log_file.readlines()
         total_count = len(lines)
@@ -76,48 +84,23 @@ def main():
         print 'Count of messages to be add into database:', total_count
 
         print 'Adding lines to database...'
-        count = dict()
-        message = dict()
         percent = 0
         i = 0
-        add_message = ("INSERT INTO {database_name} "
-                       "(`message_id`,`message_type`,`message_time`,`message_command`,"
-                       "`message_fullname`,`message_additional_information`,`message_string`) "
-                       "VALUES (%(message_id)s, %(message_type)s, %(message_time)s, %(message_command)s, "
-                       "%(message_fullname)s, %(message_additional_information)s, %(message_string)s) ".format(
-                       database_name=database_name
-                       ))
-
         for line in lines:
             i += 1
             cur_percent = i*100/total_count
             if cur_percent > percent:
                 percent = int(cur_percent)
                 print "[{percent}%]".format(percent=percent)
-                connect.commit()
+                engine.commit_connect()
 
             message = Message()
-            message.parse(message_string)
+            message.parse(line)
+            engine.insert_message(message)
 
-            message_data = {
-                'database_name': database_name,
-                'message_id': message.message_id,
-                'message_type': message.message_type,
-                'message_time': message.message_time,
-                'message_command': message.message_command,
-                'message_fullname': message.message_fullname,
-                'message_additional_information': message.message_additional_information,
-                'message_string': message.message_string
-            }
-            try:
-                cursor.execute(add_message, message_data)
-            except mysql.connector.errors.ProgrammingError:
-                print cursor.statement
-                raise
-
-        connect.commit()
-        cursor.close()
-        connect.close()
+        engine.commit_connect()
+        engine.close_cursor()
+        engine.close_connect()
 
     print datetime.now()
 
---
+++ sms_log_collector/database_engine.py
@@ -0,0 +1,109 @@
+import mysql.connector
+from sms_log_collector.message import Message
+
+
+class DatabaseEngine(object):
+    def __init__(self):
+        self.connect = None
+        self.cursor = None
+        self.table_name = None
+
+    def create_connect(self, config):
+        self.connect = mysql.connector.connect(
+            user=config['user'],
+            password=config['password'],
+            host=config['host'],
+            database=config['database']
+        )
+        self.table_name = config['table_name']
+
+    def close_connect(self):
+        self.connect.close()
+
+    def create_cursor(self):
+        self.cursor = self.connect.cursor()
+        return self.cursor
+
+    def close_cursor(self):
+        self.cursor.close()
+        self.cursor = None
+
+    def commit_connect(self):
+        self.connect.commit()
+
+    def insert_message(self, message):
+        query = ("INSERT INTO {table_name} "
+                 "(`message_id`,`message_type`,`message_time`,`message_command`,"
+                 "`message_fullname`,`message_additional_information`,`message_string`) "
+                 "VALUES (%(message_id)s, %(message_type)s, %(message_time)s, %(message_command)s, "
+                 "%(message_fullname)s, %(message_additional_information)s, %(message_string)s) ".format(
+                 table_name=self.table_name))
+
+        message_data = {
+            'table_name': self.table_name,
+            'message_id': message.message_id,
+            'message_type': message.message_type,
+            'message_time': message.message_time,
+            'message_command': message.message_command,
+            'message_fullname': message.message_fullname,
+            'message_additional_information': message.message_additional_information,
+            'message_string': message.message_string
+        }
+        try:
+            self.cursor.execute(query, message_data)
+        except mysql.connector.errors.ProgrammingError:
+            print self.cursor.statement
+            raise
+
+    def update_message(self, message):
+        query = "UPDATE {table_name} SET message_fullname = %(message_fullname)s " \
+                "WHERE message_id = %(message_id)s".format(
+                table_name=self.table_name)
+        message_data = {
+            'table_name': self.table_name,
+            'message_id': message.message_id,
+            'message_type': message.message_type,
+            'message_time': message.message_time,
+            'message_command': message.message_command,
+            'message_fullname': message.message_fullname,
+            'message_additional_information': message.message_additional_information,
+            'message_string': message.message_string
+        }
+        try:
+            self.cursor.execute(query, message_data)
+        except mysql.connector.errors.ProgrammingError:
+            print self.cursor.statement
+            raise
+
+    def get_message_count(self):
+        query = ("SELECT COUNT(message_id) FROM {table_name}".format(table_name=self.table_name))
+        self.cursor.execute(query)
+        count = -1
+        for (count,) in self.cursor:
+            pass
+        return count
+
+    def get_message(self, where_string="", order_by_string=""):
+        query = "SELECT `message_id`,`message_type`,`message_time`,`message_command`, " \
+                "`message_fullname`,`message_additional_information`,`message_string` " \
+                "FROM {table_name} ".format(table_name=self.table_name)
+        if len(where_string) > 0:
+            query += where_string + ' '
+        if len(order_by_string) > 0:
+            query += order_by_string + ' '
+        q = (query,)
+        self.cursor.execute(query)
+        message_list = []
+        for (message_id, message_type, message_time, message_command, message_fullname,
+             message_additional_information, message_string) in self.cursor:
+            message = Message()
+            message.message_id = message_id
+            message.message_type = message_type
+            message.message_time = message_time
+            message.message_command = message_command
+            message.message_fullname = message_fullname
+            message.message_additional_information = message_additional_information
+            message.message_string = message_string
+            message_list.append(message)
+
+        return message_list
