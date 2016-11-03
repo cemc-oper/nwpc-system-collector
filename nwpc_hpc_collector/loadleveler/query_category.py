6d0456c45c622c72fce627820508f25f4e3919f6
perillaroc <perillaroc@gmail.com>
2016-11-03 13:47:16 +0800

    add constructor in QueryCategory.

--- nwpc_hpc_collector/loadleveler/query_category.py
+++ nwpc_hpc_collector/loadleveler/query_category.py
@@ -2,16 +2,36 @@ import copy
 
 
 class QueryCategory(object):
-    def __init__(self):
-        self.id = ".valid_id"
+    def __init__(
+            self,
+            category_id=".valid_id",
+            display_name="",
+            label="",
+            record_parser_class=None,
+            record_parser_arguments=tuple(),
+            value_saver_class=None,
+            value_saver_arguments=tuple()
+    ):
+        self.id = category_id
+        self.display_name = display_name
+        self.label = label
 
         self.record_parser = None
         self.value_saver = None
+
+        if record_parser_class is not None:
+            self.record_parser = record_parser_class(*record_parser_arguments)
+
+        if value_saver_class is not None:
+            self.value_saver = value_saver_class(*value_saver_arguments)
 
     def __deepcopy__(self, memodict={}):
         new_object = QueryCategory()
 
         new_object.id = self.id
+        new_object.display_name = self.display_name
+        new_object.label = self.label
+
         if self.record_parser:
             new_object.record_parser = copy.deepcopy(self.record_parser)
 
--- test/auto/nwpc_hpc_collector/loadleveler/query_category/test_query_category.py
+++ test/auto/nwpc_hpc_collector/loadleveler/query_category/test_query_category.py
@@ -29,3 +29,20 @@ class TestQueryCategory(unittest.TestCase):
         self.assertNotEqual(category.id, new_category.id)
         self.assertNotEqual(category.record_parser, new_category.record_parser)
         self.assertNotEqual(category.record_parser.label, new_category.record_parser.label)
+
+    def test_create_query_category(self):
+        id_category = query_category.QueryCategory(
+            category_id="llq.id",
+            display_name="Id",
+            label="Job Step Id",
+            record_parser_class=record_parser.DetailLabelParser,
+            record_parser_arguments=("Job Step Id",),
+            value_saver_class=value_saver.StringSaver
+        )
+
+        self.assertEqual(id_category.id, "llq.id")
+        self.assertEqual(id_category.display_name, "Id")
+        self.assertEqual(id_category.label, "Job Step Id")
+        self.assertIsInstance(id_category.record_parser, record_parser.DetailLabelParser)
+        self.assertEqual(id_category.record_parser.label, "Job Step Id")
+        self.assertIsInstance(id_category.value_saver, value_saver.StringSaver)
