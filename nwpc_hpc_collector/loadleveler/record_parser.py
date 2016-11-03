5a069d9fa70734e0e526d33bfe39fe662fc9c57a
perillaroc <perillaroc@gmail.com>
2016-11-03 09:57:34 +0800

    add test data and add DetailRecordParser.

---
+++ nwpc_hpc_collector/loadleveler/record_parser.py
@@ -0,0 +1,29 @@
+# coding=utf-8
+
+
+class RecordParser(object):
+    def __init__(self):
+        self.type = self.__class__
+
+    def parse(self, record):
+        pass
+
+
+class DetailLabelParser(RecordParser):
+    def __init__(self, label):
+        RecordParser.__init__(self)
+        self.type = self.__class__
+
+        self.label = label
+
+    def parse(self, record):
+        for line in record:
+            index = line.find(': ')
+            if index == -1:
+                continue
+            label = line[0:index].strip()
+            if label != self.label:
+                continue
+            value = line[index+2: -1].strip()
+            return value
+        return ""
---
+++ test/auto/nwpc_hpc_collector/loadleveler/record_parser/test_record_parser.py
@@ -0,0 +1,70 @@
+# coding=utf-8
+import unittest
+from unittest import mock
+import os
+import importlib
+
+from nwpc_hpc_collector.loadleveler import record_parser
+
+
+class TestRecordParser(unittest.TestCase):
+    def setUp(self):
+        importlib.reload(record_parser)
+
+    def tearDown(self):
+        pass
+
+    def check_llq_detail_query_record_parser(self, test_case):
+        lines = test_case["lines"]
+        label = test_case["label"]
+        value = test_case["value"]
+        name = test_case["name"]
+
+        parser = record_parser.DetailLabelParser(label)
+        parser_value = parser.parse(lines)
+        self.assertEqual(parser_value, value)
+        print("Test passed:", name)
+
+    def test_llq_detail_query_record_parser(self):
+        serial_job_running_file_path = os.path.join(
+            os.path.dirname(__file__),
+            "data/detail_query/llq/serial_job_running.txt"
+        )
+        test_case_list = []
+        with open(serial_job_running_file_path) as serial_job_running_file:
+            lines = serial_job_running_file.readlines()
+            test_case_list.extend([
+                {
+                    "name": "llq.serial_job.running.job_step_id",
+                    "lines": lines,
+                    "label": "Job Step Id",
+                    "value": "cma20n04.2882680.0"
+                },
+                {
+                    "name": "llq.serial_job.running.cmd",
+                    "lines": lines,
+                    "label": "Cmd",
+                    "value": "/cma/g1/nwp/SMSOUT/gmf_grapes_gfs_v2_0/grapes_global/12/post/postp_240.job1"
+                },
+                {
+                    "name": "llq.serial_job.running.queue_date",
+                    "lines": lines,
+                    "label": "Queue Date",
+                    "value": "Thu Sep  8 00:09:02 2016"
+                },
+                {
+                    "name": "llq.serial_job.running.cpu_per_core",
+                    "lines": lines,
+                    "label": "Cpus Per Core",
+                    "value": "0"
+                },
+                {
+                    "name": "llq.serial_job.running.task_affinity",
+                    "lines": lines,
+                    "label": "Task Affinity",
+                    "value": ""
+                },
+            ])
+
+        for a_test_case in test_case_list:
+            self.check_llq_detail_query_record_parser(a_test_case)
