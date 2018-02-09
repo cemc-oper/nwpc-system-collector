b25cfbaa35a7b9390d8f735f7a12b83471258868
perillaroc <perillaroc@gmail.com>
2018-02-09 11:09:00 +0800

    add nwpc-ecflow-collector

---
+++ nwpc-ecflow-collector/setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='nwpc-ecflow-collector',
+
+    version='0.1.0',
+
+    description='An ecFlow collector at NWPC.',
+    long_description=long_description,
+
+    url='https://github.com/perillaroc/nwpc-log-collector',
+
+    author='perillaroc',
+    author_email='perillaroc@gmail.com',
+
+    license='MIT',
+
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6'
+    ],
+
+    keywords='nwpc ecflow collector',
+
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+
+    install_requires=[
+        'click',
+        'PyYAML'
+    ],
+
+    extras_require={
+        'test': ['pytest'],
+    },
+
+    entry_points={}
+)
