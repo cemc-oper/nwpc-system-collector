a00bc0bcb57412accaf20efa3bca4389a40e4c75
perillaroc <perillaroc@gmail.com>
2018-11-23 14:30:02 +0800

    :art: add status config option in server cli.
    :truck: move proto files into proto directory.

--- nwpc-ecflow-collector/nwpc_ecflow_collector/server/ecflow_collector_client.py
+++ nwpc-ecflow-collector/nwpc_ecflow_collector/server/ecflow_collector_client.py
@@ -1,18 +1,8 @@
 # coding: utf-8
 import grpc
-
 import click
-import yaml
 
-
-import ecflow_collector_pb2
-import ecflow_collector_pb2_grpc
-
-
-def get_config(config_file_path):
-    with open(config_file_path) as config_file:
-        config = yaml.load(config_file)
-        return config
+from nwpc_ecflow_collector.server.proto import ecflow_collector_pb2_grpc, ecflow_collector_pb2
 
 
 @click.group()
@@ -21,29 +11,28 @@ def cli():
 
 
 @cli.command()
+@click.option('--rpc-target', '-t', help='rpc target')
 @click.option('--owner', '-o', help='owner name', required=True)
 @click.option('--repo', '-r', help='repo name', required=True)
 @click.option('--host', help='ecflow host, ECF_HOST')
 @click.option('--port', help='ecflow port, ECF_PORT')
-@click.option('--config', '-c', 'config_file_path', help='config file path')
 @click.option('--disable-post', is_flag=True, help='disable post to agent', default=False)
 @click.option('--post-url', help='post URL')
 @click.option('--gzip', 'content_encoding', flag_value='gzip', help='use gzip to post data.')
 @click.option('--verbose', is_flag=True, help='show more outputs', default=False)
-def collect(owner, repo, host, port, config_file_path, disable_post, post_url, content_encoding, verbose):
+def collect(rpc_target, owner, repo, host, port, disable_post, post_url, content_encoding, verbose):
     status_request = ecflow_collector_pb2.StatusRequest(
         owner=owner,
         repo=repo,
         host=host,
         port=port,
-        config=config_file_path,
         post_url=post_url,
         content_encoding=content_encoding,
         disable_post=disable_post,
         verbose=verbose
     )
 
-    with grpc.insecure_channel('localhost:50051') as channel:
+    with grpc.insecure_channel(rpc_target) as channel:
         stub = ecflow_collector_pb2_grpc.EcflowCollectorStub(channel)
         response = stub.CollectStatus(status_request)
         print(response.status)
--- nwpc-ecflow-collector/nwpc_ecflow_collector/server/ecflow_collector_server.py
+++ nwpc-ecflow-collector/nwpc_ecflow_collector/server/ecflow_collector_server.py
@@ -1,53 +1,76 @@
 # coding: utf-8
 from concurrent import futures
 import time
-from nwpc_ecflow_collector.ecflow_status_collector import collect_status
+import logging
 
+import click
 import grpc
 
-import ecflow_collector_pb2
-import ecflow_collector_pb2_grpc
+from nwpc_ecflow_collector.ecflow_status_collector import collect_status
+from nwpc_ecflow_collector.server.proto import ecflow_collector_pb2_grpc, ecflow_collector_pb2
 
 _ONE_DAY_IN_SECONDS = 60 * 60 * 24
 
+logging.basicConfig(
+    format='%(asctime)s %(message)s',
+    datefmt='[%Y-%m-%d %H:%M:%S]',
+    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
 
 class EcflowCollectorService(ecflow_collector_pb2_grpc.EcflowCollectorServicer):
-    def __init__(self):
-        pass
+    def __init__(self, status_config_file_path):
+        self.status_config_file_path = status_config_file_path
 
     def CollectStatus(self, request, context):
         owner = request.owner
         repo = request.repo
         host = request.host
         port = request.port
+
         config_file_path = request.config
+        if len(config_file_path) == 0:
+            config_file_path = self.status_config_file_path
+
         post_url = request.post_url
         content_encoding = request.content_encoding
         disable_post = request.disable_post
         verbose = request.verbose
 
+        logger.info('CollectStatus: {owner}/{repo}...'.format(owner=owner, repo=repo))
         collect_status(
             owner, repo, host, port, config_file_path,
             disable_post, post_url, content_encoding,
             verbose
         )
+        logger.info('CollectStatus: {owner}/{repo}...done'.format(owner=owner, repo=repo))
 
         return ecflow_collector_pb2.Response(
             status="ok"
         )
 
 
-def serve():
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+@click.command()
+@click.option('-t', '--rpc-target', help='rpc-target', default="[::]:50051")
+@click.option('-n', '--workers-num', help='max workers number', default=5, type=int)
+@click.option('--status-config', 'status_config_file_path',
+              help="status collector's config file path")
+def serve(rpc_target, workers_num, status_config_file_path):
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=workers_num))
     ecflow_collector_pb2_grpc.add_EcflowCollectorServicer_to_server(
-        EcflowCollectorService(), server)
-    server.add_insecure_port('[::]:50051')
+        EcflowCollectorService(status_config_file_path), server)
+    server.add_insecure_port('{rpc_target}'.format(rpc_target=rpc_target))
+    logger.info('listening on {rpc_target}'.format(rpc_target=rpc_target))
+    logger.info('starting server...')
     server.start()
     try:
         while True:
             time.sleep(_ONE_DAY_IN_SECONDS)
     except KeyboardInterrupt:
+        logger.info('warm stop...')
         server.stop(0)
+        logger.info('warm stop...done')
 
 
 if __name__ == "__main__":
